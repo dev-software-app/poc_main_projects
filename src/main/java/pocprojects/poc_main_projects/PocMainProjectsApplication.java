5d34e3a64d7e8842aa3e8732fe89d9b188e1da47
Yada dev <yadadev.io@gmail.com>
2022-04-08 20:43:34 -0400

    Init project

---
+++ src/main/java/pocprojects/poc_main_projects/PocMainProjectsApplication.java
@@ -0,0 +1,13 @@
+package pocprojects.poc_main_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMainProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMainProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pocprojects/poc_main_projects/PocMainProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package pocprojects.poc_main_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMainProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
